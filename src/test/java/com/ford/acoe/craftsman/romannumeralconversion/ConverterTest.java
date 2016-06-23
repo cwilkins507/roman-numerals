fd5647913a46a6dcfded0937e79c3a25290ffa4b
Mike Smithson <msmithso@ford.com>
2016-06-23 17:19:49 -0400

    Initial commit of project

---
+++ src/test/java/com/ford/acoe/craftsman/romannumeralconversion/ConverterTest.java
@@ -0,0 +1,15 @@
+package com.ford.acoe.craftsman.romannumeralconversion;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConverterTest {
+	
+	@Test
+	public void testSomething() {
+		assertThat(true).isFalse();
+	}
+
+}
