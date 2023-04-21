d10083d3415e7cd6941340723df0e393e6ec1b8d
Collin Wilkins <cwilki32@ford.com>
2023-04-21 11:29:28 -0400

    tdd solution
    Co-authored-by: Michael Smithson <msmithso@ford.com>

--- src/main/java/com/ford/csoe/roman/numerals/Converter.java
+++ src/main/java/com/ford/csoe/roman/numerals/Converter.java
@@ -1,9 +1,42 @@
 package com.ford.csoe.roman.numerals;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Converter implements RomanNumeralConverter {
 
-	@Override
-	public String convert(Integer romanNumeral) {
-		return null;
-	}
+    private static Map<Integer, String> ARABIC_TO_ROMAN = new LinkedHashMap<>();
+
+    static {
+        ARABIC_TO_ROMAN.put(1000, "M");
+        ARABIC_TO_ROMAN.put(900, "CM");
+        ARABIC_TO_ROMAN.put(500, "D");
+        ARABIC_TO_ROMAN.put(400, "CD");
+        ARABIC_TO_ROMAN.put(100, "C");
+        ARABIC_TO_ROMAN.put(90, "XC");
+        ARABIC_TO_ROMAN.put(50, "L");
+        ARABIC_TO_ROMAN.put(40, "XL");
+        ARABIC_TO_ROMAN.put(10, "X");
+        ARABIC_TO_ROMAN.put(9, "IX");
+        ARABIC_TO_ROMAN.put(6, "VI");
+        ARABIC_TO_ROMAN.put(5, "V");
+        ARABIC_TO_ROMAN.put(4, "IV");
+        ARABIC_TO_ROMAN.put(1, "I");
+    }
+
+    @Override
+    public String convert(Integer romanNumeral) {
+        StringBuilder s = new StringBuilder();
+        int remaining = romanNumeral;
+        while (remaining > 0) {
+            for (Map.Entry<Integer, String> entry : ARABIC_TO_ROMAN.entrySet()) {
+                if (entry.getKey() <= remaining) {
+                    remaining -= entry.getKey();
+                    s.append(entry.getValue());
+                    break;
+                }
+            }
+        }
+        return s.toString();
+    }
 }
--- src/test/java/com/ford/csoe/roman/numerals/ConverterTest.java
+++ src/test/java/com/ford/csoe/roman/numerals/ConverterTest.java
@@ -7,9 +7,104 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class ConverterTest {
+
+	private final Converter c = new Converter();
 	
 	@Test
-	public void testSomething() {
-		assertThat(true).isFalse();
+	public void given1_shouldReturnI() {
+		String expected = "I";
+
+		assertThat(c.convert(1)).isEqualTo(expected);
 	}
+
+	@Test
+	public void given2_shouldReturnII(){
+		String expected = "II";
+		assertThat(c.convert(2)).isEqualTo(expected);
+	}
+
+	@Test
+	public void given3_shouldReturnIII(){
+		String expected = "III";
+		assertThat(c.convert(3)).isEqualTo(expected);
+	}
+
+	@Test
+	public void given4_shouldReturnIV(){
+		String expected = "IV";
+		assertThat(c.convert(4)).isEqualTo(expected);
+	}
+
+	@Test
+	public void given5_shouldReturnV(){
+		String expected = "V";
+		assertThat(c.convert(5)).isEqualTo(expected);
+	}
+
+	@Test
+	public void given6_shouldReturnVI(){
+		String expected = "VI";
+		assertThat(c.convert(6)).isEqualTo(expected);
+	}
+
+	@Test
+	public void given7_shouldReturnVII(){
+		String expected = "VII";
+		assertThat(c.convert(7)).isEqualTo(expected);
+	}
+
+	@Test
+	public void given8_shouldReturnVIII(){
+		String expected = "VIII";
+		assertThat(c.convert(8)).isEqualTo(expected);
+	}
+
+	@Test
+	public void given9_shouldReturnIX(){
+		String expected = "IX";
+		assertThat(c.convert(9)).isEqualTo(expected);
+	}
+
+	@Test
+	public void given10_shouldReturnX(){
+		String expected = "X";
+		assertThat(c.convert(10)).isEqualTo(expected);
+	}
+
+	@Test
+	public void given11_shouldReturnXI(){
+		String expected = "XI";
+		assertThat(c.convert(11)).isEqualTo(expected);
+	}
+
+	@Test
+	public void given14_shouldReturnXIV(){
+		String expected = "XIV";
+		assertThat(c.convert(14)).isEqualTo(expected);
+	}
+
+	@Test
+	public void given15_shouldReturnVVV(){
+		String expected = "XV";
+		assertThat(c.convert(15)).isEqualTo(expected);
+	}
+
+	@Test
+	public void given16_shouldReturnVVV(){
+		String expected = "XVI";
+		assertThat(c.convert(16)).isEqualTo(expected);
+	}
+
+	@Test
+	public void given19_shouldReturnXIX(){
+		String expected = "XIX";
+		assertThat(c.convert(19)).isEqualTo(expected);
+	}
+
+	@Test
+	public void given20_shouldReturnXX(){
+		String expected = "XX";
+		assertThat(c.convert(20)).isEqualTo(expected);
+	}
+
 }
